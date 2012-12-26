9b7ceac90481db556e5021e898f8ee33e583b450
matthewkeehner <matthewkeehner@gmail.com>
2012-12-26 14:21:23 -0600

    Finished implementing TinyMCE js plugin functionality.

--- js/highlight_shortcode.js
+++ js/highlight_shortcode.js
@@ -1,16 +1,39 @@
 jQuery(document).ready(function($) {
   tinymce.create('tinymce.plugins.tnihs_plugin', {
     init : function( ed, url ) {
+
+      // Variable to store if the short tag is open or not
+      var open = false;
+
       // Register command for when button is clicked
-      ed.addCommand( 'tnihs_insert_shortcode', function() {
+      ed.addCommand( 'tnihs_insert_shortcode', function(u, v) {
         selected = tinyMCE.activeEditor.selection.getContent();
 
+
         if( selected ){
-          //If text is selected when button is clicked
-          //Wrap shortcode around it.
-          content = '[highlight]' + selected + '[/highlight]';
-        }else{
+          // If text is selected when button is clicked, wrap shortcode around it
+
+          // Reset content
+          content = '';
+
+          // Close other tag if currently open.
+          if ( open ) {
+            content = '[/highlight]'
+          }
+
+          // Wrap selected
+          content += '[highlight]' + selected + '[/highlight]';
+
+          // Reset open
+          open = false;
+        }else if( ! open ) {
+          // Open highlight tag
           content = '[highlight]';
+          open = true;
+        }else {
+          // Close highlight tag
+          content = '[/highlight]';
+          open = false;
         }
 
         tinymce.execCommand( 'mceInsertContent', false, content );
@@ -20,8 +43,9 @@ jQuery(document).ready(function($) {
       ed.addButton( 'tnihs_button', {
         title : 'Insert shortcode',
         cmd : 'tnihs_insert_shortcode',
-        image: url + '/path/to/image.png'
+        image : url + '/path/to/image.png'
       });
+
     }
   });
 
