39c6d5a98438f13513933ad50fb06a40bbbf8264
matthewkeehner <matthewkeehner@gmail.com>
2012-12-26 11:33:51 -0600

    Inital commit.  Setup plugin.

---
+++ highlight_shortcode.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package Highlight_Shortcode
+ */
+/*
+Plugin Name: Highlight Shortcode
+Plugin URI:
+Description: Adds a highlight wrapper to content.
+Version: 0.1
+Author: The Net Impact
+Author URI: http://www.thenetimpact.com
+License: GPLv2 or later
+*/
+
+?>
